23f657d7eff53c7050b0b9d3fecdeb5cf937679c
GaryFoYDev <garyfoy@hotmail.fr>
2024-04-20 15:31:04 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+##############################################################################
+# USIDOF Project                                                             #
+#                                                                            #
+# This file contains the code required to preprocess the data                #
+#                                                                            #
+#                                                                            #
+# Author: Gary FOY                                                           #
+# Date: 20-04-2024                                                           #
+# Email: gary.foy.auditeur@lecnam.net                                        #
+##############################################################################
+
+import numpy as np
+import pandas as pd
+    
